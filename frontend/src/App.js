9562925894372eaafae60a09b06253370c62a134
ChrisEski <eschristos1@gmail.com>
2022-04-05 14:15:59 +0300

    Video-06-Add-Page-Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,25 @@
-import data from "./data";
+// Tools
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+// Components
+import HomeScreen from "./screens/Homescreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map(product => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add To Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
