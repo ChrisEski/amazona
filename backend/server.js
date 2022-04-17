6f127e3bd282e2fe8134a9d79864c7a1738725f7
ChrisEski <eschristos1@gmail.com>
2022-04-17 17:08:19 +0300

    initial heroku commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-import data from "./data.js";
-
 // Tools
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+// import path from "path";
 
 // Routers
 import seedRouter from "./routes/seedRoutes.js";
@@ -31,6 +30,12 @@ app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(expres.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
